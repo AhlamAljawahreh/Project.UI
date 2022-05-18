1f2eaefa3e5b6f8589a979e16c1ca0f8bcb30e73
AhlamAljawahreh <ahlam.aljawahreh@gmail.com>
2022-05-18 21:07:58 +0300

    Add project files.

---
+++ Services/IProductServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Project.DataModels;
+
+namespace Project.UI.Services
+{
+    public interface IProductServices
+    {
+        Task<IEnumerable<Product>> GetProducts();
+    }
+}
---
+++ Services/ProductServices.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Project.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Project.UI.Services
+{
+    public class ProductServices : IProductServices
+    {
+        private HttpClient httpClient;
+        public ProductServices(HttpClient _httpClient)
+        {
+            this.httpClient = _httpClient;
+        }
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await httpClient.GetJsonAsync<Product[]>("api/products");
+        }
+
+    }
+}
